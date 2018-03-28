2f1d1d1bf7a287b0b4148809277a043d85a8f06c
robertbutacu <robert.butacu@yahoo.com>
2018-03-29 00:57:08 +0300

    trait Expression - will prolly use type classes

--- src/main/scala/parser/function/elements/Expression.scala
+++ src/main/scala/parser/function/elements/Expression.scala
@@ -2,7 +2,7 @@ package parser.function.elements
 
 import parser.function.elements.fundamental.Argument
 
-abstract class Expression[A] {
+trait Expression[A] {
   def compute: Option[A]
   def mapConstants[B: Numeric](f: A => B)
   def replaceArgument(old: Argument[A], updated: Argument[A])
