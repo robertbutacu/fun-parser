4d1fe0d5233222bfd994208c889668ee87840861
robertbutacu <robertbutacu@gmail.com>
2019-01-06 22:31:42 +0000

    small optimisation for Divide - right side no longer computed twice

--- src/main/scala/parser/function/elements/expressions/Divide.scala
+++ src/main/scala/parser/function/elements/expressions/Divide.scala
@@ -1,14 +1,20 @@
 package parser.function.elements.expressions
 
 import parser.function.elements.Expression
-import parser.function.elements.fundamental.Argument
+import parser.function.elements.fundamental.{Argument, Constant}
 
 case class Divide[A: Fractional](dividend: Expression[A], divisor: Expression[A]) extends Expression[A] {
   override def compute(implicit n: Fractional[A]): Either[String, A] = {
     val divisorResult = divisor.compute
 
+    def go(): Either[String, A] =
+      for {
+        right  <- divisorResult
+        result <- computeBy(dividend, Constant("", right), n.div)
+      } yield result
+
     if(divisorResult.contains(n.zero)) Left("Cannot divide by zero")
-    else                               computeBy(dividend, divisor, n.div)
+    else                               go()
   }
 
   override def mapConstants[B: Fractional](f: A => B)(implicit n: Fractional[A]): Expression[A] = ???
