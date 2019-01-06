24710b16ce53e65f8b3dafe91359dba4dc49c369
robertbutacu <robertbutacu@gmail.com>
2019-01-06 22:01:54 +0000

    B is now fractional aswell

--- src/main/scala/parser/Parser.scala
+++ src/main/scala/parser/Parser.scala
@@ -1,5 +1,4 @@
 package parser
 
 object Parser {
-
 }
--- src/main/scala/parser/function/elements/fundamental/Argument.scala
+++ src/main/scala/parser/function/elements/fundamental/Argument.scala
@@ -5,7 +5,7 @@ import parser.function.elements.Expression
 case class Argument[A: Fractional](name: String, value: Option[A] = None) extends Expression[A] {
   override def compute(implicit n: Fractional[A]): Option[A] = ???
 
-  override def mapConstants[B: Numeric](f: A => B)(implicit n: Fractional[A]): Unit = ???
+  override def mapConstants[B: Fractional](f: A => B)(implicit n: Fractional[A]): Unit = ???
 
   override def replaceArgument(old: Argument[A], updated: Argument[A])(implicit n: Fractional[A]): Unit = ???
 }
